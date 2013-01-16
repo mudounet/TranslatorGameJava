23f300dedc04631e9a606651457623a2f25de6bd
mudounet <mudounet@free.fr>
2013-01-16 23:07:03 +0400

    Changed formatting.

--- src/main/java/com/mudounet/xml/stats/MeanList.java
+++ src/main/java/com/mudounet/xml/stats/MeanList.java
@@ -6,25 +6,30 @@ import org.simpleframework.xml.Root;
 
 @Root
 public class MeanList {
-	private static final int MAX_ITEMS = 6;
-	
-	@ElementList(inline=true, required=false)
-	private ArrayList<Float> list = new ArrayList<Float>();
-	
-	public void add(float value) {
-		if(list.size() >= MAX_ITEMS) list.remove(0);
-		
-		list.add(value);
-	}
-	
-	public float mean() {
-		float mean = 0.0f;
-		
-		if (list.size() == 0) return 0;
-		
-		for (int index = 0; index < list.size(); index++)
-			mean += list.get(index);
-		
-		return mean / list.size();	
-	}
+
+    private static final int MAX_ITEMS = 6;
+    @ElementList(inline = true, required = false)
+    private ArrayList<Float> list = new ArrayList<Float>();
+
+    public void add(float value) {
+        if (list.size() >= MAX_ITEMS) {
+            list.remove(0);
+        }
+
+        list.add(value);
+    }
+
+    public float mean() {
+        float mean = 0.0f;
+
+        if (list.isEmpty()) {
+            return 0;
+        }
+
+        for (int index = 0; index < list.size(); index++) {
+            mean += list.get(index);
+        }
+
+        return mean / list.size();
+    }
 }
